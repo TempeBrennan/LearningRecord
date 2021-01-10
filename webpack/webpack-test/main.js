f5bf1f8bbd316acd924f39ba96864709d1507731
TempeBrennan <425751342@qq.com>
2021-01-10 10:58:40 +0800

    init

---
+++ webpack/webpack-test/main.js
@@ -0,0 +1,18 @@
+// import $ from 'jquery';
+
+// $.getJSON("test.json", function (json) {
+//     console.log(json);
+// });
+
+import './res/styles.css';
+
+import imageRes from './res/test.jpg';
+const img = new Image();
+img.src = imageRes;
+document.body.appendChild(img);
+
+import xml from './res/test.xml';
+import json from './res/test.json';
+
+console.log(xml);
+console.log(json);
---
+++ webpack/webpack-test/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'main.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [{
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(csv|tsv)$/i,
+                use: ['csv-loader'],
+            },
+            {
+                test: /\.xml$/i,
+                use: ['xml-loader'],
+            },
+        ],
+    },
+    mode: 'development',
+    devtool: 'source-map',
+    target: ['web', 'es5'],
+}
